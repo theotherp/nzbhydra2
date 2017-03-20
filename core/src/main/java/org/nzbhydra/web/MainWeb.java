ccacb713b53b2cda90ca60e1804baa70f970db73
theotherp@gmx.de <SFSu3uR#qe&n#Y7lBC*J>
2017-03-20 21:03:40 +0100

    Add thymeleaf template, got fucking bootstrapping variables working

--- core/src/main/java/org/nzbhydra/web/MainWeb.java
+++ core/src/main/java/org/nzbhydra/web/MainWeb.java
@@ -3,11 +3,19 @@ package org.nzbhydra.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 public class MainWeb {
 
     @RequestMapping("/")
-    public String index() {
+    public String index(HttpSession session) {
+        session.setAttribute("baseUrl", "/");
+        Map<String, String> map = new HashMap<>();
+        map.put("key", "value");
+        session.setAttribute("bootstrap", map);
         return "index";
     }
 
