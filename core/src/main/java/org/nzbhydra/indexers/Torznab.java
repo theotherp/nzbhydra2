6802f10d008919431c0540fc0b2f9326026f5dca
theotherp@gmx.de <SFSu3uR#qe&n#Y7lBC*J>
2017-07-30 15:02:00 +0200

    Fix torznab result processing

--- core/src/main/java/org/nzbhydra/indexers/Torznab.java
+++ core/src/main/java/org/nzbhydra/indexers/Torznab.java
@@ -3,13 +3,22 @@ package org.nzbhydra.indexers;
 import lombok.Getter;
 import lombok.Setter;
 import org.nzbhydra.mapping.newznab.NewznabAttribute;
+import org.nzbhydra.mapping.newznab.RssChannel;
 import org.nzbhydra.mapping.newznab.RssItem;
+import org.nzbhydra.mapping.newznab.RssRoot;
+import org.nzbhydra.mapping.newznab.Xml;
+import org.nzbhydra.searching.IndexerSearchResult;
+import org.nzbhydra.searching.ResultAcceptor.AcceptorResult;
+import org.nzbhydra.searching.SearchResultIdCalculator;
 import org.nzbhydra.searching.SearchResultItem;
 import org.nzbhydra.searching.SearchResultItem.DownloadType;
 import org.nzbhydra.searching.SearchResultItem.HasNfo;
+import org.nzbhydra.searching.searchrequests.SearchRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.util.Collections;
 
 @Getter
 @Setter
@@ -21,10 +30,10 @@ public class Torznab extends Newznab {
     protected SearchResultItem createSearchResultItem(RssItem item) {
         item.getRssGuid().setPermaLink(true); //Not set in RSS but actually always true
         SearchResultItem searchResultItem = super.createSearchResultItem(item);
-
-        //TODO: Category is in main category tag, not in attributes
+        computeCategory(searchResultItem, Collections.singletonList(Integer.valueOf(item.getCategory())));
 
         searchResultItem.setGrabs(item.getGrabs());
+        searchResultItem.setIndexerGuid(item.getRssGuid().getGuid());
         for (NewznabAttribute attribute : item.getTorznabAttributes()) {
             searchResultItem.getAttributes().put(attribute.getName(), attribute.getValue());
             if (attribute.getName().equals("grabs")) {
@@ -35,8 +44,19 @@ public class Torznab extends Newznab {
         }
         searchResultItem.setHasNfo(HasNfo.NO);
         searchResultItem.setDownloadType(DownloadType.TORRENT);
-
+        searchResultItem.setGuid(SearchResultIdCalculator.calculateSearchResultId(searchResultItem));
         return searchResultItem;
+    }
+
+    @Override
+    protected void completeIndexerSearchResult(Xml response, IndexerSearchResult indexerSearchResult, AcceptorResult acceptorResult, SearchRequest searchRequest) {
+        RssChannel rssChannel = ((RssRoot) response).getRssChannel();
+        super.completeIndexerSearchResult(response, indexerSearchResult, acceptorResult, searchRequest);
+        indexerSearchResult.setTotalResultsKnown(true);
+        indexerSearchResult.setHasMoreResults(false);
+        indexerSearchResult.setOffset(0);
+        indexerSearchResult.setTotalResults(rssChannel.getItems().size());
+        indexerSearchResult.setLimit(searchRequest.getLimit().orElse(100));
     }
 
     protected Logger getLogger() {
