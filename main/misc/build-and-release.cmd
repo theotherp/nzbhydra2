19b499f276df3a416851ed58a3a208786194fd45
theotherp@gmx.de <SFSu3uR#qe&n#Y7lBC*J>
2017-09-16 09:27:41 +0200

    Add build script

---
+++ main/misc/build-and-release.cmd
@@ -0,0 +1,68 @@
+@echo off
+if "%1" == "" (
+    echo Release version missing
+    goto error
+)
+if "%2" == "" (
+    echo New snapshot version missing
+    goto error
+)
+
+if not exist changelog.json (
+    echo Not running in main folder
+    goto error
+)
+
+echo Generating changelog
+call mvn github-release:generate-changelog
+if not "%ERRORLEVEL%" == "0" goto error
+
+echo Setting release version
+call mvn versions:set -DnewVersion=%1
+if not "%ERRORLEVEL%" == "0" goto error
+
+echo Running clean install
+call mvn clean install
+if not "%ERRORLEVEL%" == "0" goto error
+
+echo Making version effective
+call mvn versions:commit
+if not "%ERRORLEVEL%" == "0" goto error
+
+cd..
+if not exist changelog.md (
+    echo Tried to switch to root folder but didn't find changelog.md
+    goto error
+)
+
+echo Committing
+call git commit -am "Update to %1"
+if not "%ERRORLEVEL%" == "0" goto error
+
+echo Tagging
+call git tag -a v%1 -m "v%1"
+if not "%ERRORLEVEL%" == "0" goto error
+
+echo Pushing
+call git push origin
+if not "%ERRORLEVEL%" == "0" goto error
+
+cd main
+if not exist changelog.json (
+    echo Tried to switch to main folder but didn't find changelog.json
+    goto error
+)
+
+echo Releasing to GitHub
+call mvn github-release:release
+if not "%ERRORLEVEL%" == "0" goto error
+
+echo Setting new snapshot version
+call mvn versions:set DnewVersion=%1-SNAPSHOT
+if not "%ERRORLEVEL%" == "0" goto error
+
+goto eof
+:error
+echo Error, aborted
+
+:eof
