50d1d00221902c79dd959705b0ddec1457ef26b1
theotherp@gmx.de <SFSu3uR#qe&n#Y7lBC*J>
2018-01-06 14:16:56 +0100

    Run windows service as local system account

--- releases/windows-release/include/WindowsService/installService.cmd
+++ releases/windows-release/include/WindowsService/installService.cmd
@@ -12,25 +12,29 @@ if %errorLevel% == 0 (
 
 :install
 echo Installing service
-call "%~dp0nssm.exe" install NzbHydra2 "%%~dp0..\nzbhydra2.exe"
+call "%~dp0nssm.exe" install NzbHydra2 "%%~dp0..\nzbhydra2 console.exe"
 if %errorlevel% neq 0 goto failure
 
 echo Setting service exe
-call "%~dp0nssm.exe" set NzbHydra2 Application "%~dp0..\nzbhydra2.exe"
+call "%~dp0nssm.exe" set NzbHydra2 Application "%~dp0..\nzbhydra2 console.exe"
 if %errorlevel% neq 0 goto failure
 
 echo Setting service folder
 call "%~dp0nssm.exe" set NzbHydra2 AppDirectory "%~dp0.."
 if %errorlevel% neq 0 goto failure
 
+echo Setting service description
+call "%~dp0nssm.exe" set NzbHydra2 Description "NZBHydra2 usenet search"
+if %errorlevel% neq 0 goto failure
+
 echo Setting service to shutdown on return code 0 of wrapper
 call "%~dp0nssm.exe" set NzbHydra2 AppExit 0 Exit
 if %errorlevel% neq 0 goto failure
 
-echo Enter your username like this: ".\username" 
-set /p username="Username:"
-set /p password="Enter your windows password:"
-call "%~dp0nssm.exe" set NzbHydra2 ObjectName %username% %password%
+rem echo Enter your username like this: ".\username"
+rem set re username="Username:"
+rem set /p password="Enter your windows password:"
+rem call "%~dp0nssm.exe" set NzbHydra2 ObjectName %username% %password%
 
 echo Service installed successfully. Starting service...
 call "%~dp0nssm.exe" start NzbHydra2
