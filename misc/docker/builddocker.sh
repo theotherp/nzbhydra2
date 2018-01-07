feb7df23e0959824a33da4741b5f2b900ce02750
theotherp@gmx.de <SFSu3uR#qe&n#Y7lBC*J>
2018-01-07 09:02:55 +0100

    Set snapshot to 1.0.10

--- misc/docker/builddocker.sh
+++ misc/docker/builddocker.sh
@@ -1,14 +1,18 @@
-rm ./lib/*.jar
+if [[ -f lib/*.jar ]] ; then
+    rm ./lib/*.jar
+fi
 cp ../../other/wrapper/nzbhydra2wrapper.py .
-#cp ../../core/target/*-exec*.jar ./lib
+cp ../../core/target/*-exec*.jar ./lib
 cp ../../readme.md .
 cp ../../changelog.md .
-rm ./lib/*SNAPSHOT*.jar
-if [[ ! -f lib/*exec*.jar ]] ; then
+if [[ -f lib/*SNAPSHOT*.jar ]] ; then
+    rm ./lib/*SNAPSHOT*.jar
+fi
+if [[ ! -f lib/*.jar ]] ; then
     echo No exec JAR in lib folder
     exit
 fi
 echo -n "Enter version"
 read VERSION
 docker build -t theotherp/nzbhydra2:${VERSION} -t theotherp/nzbhydra2:latest .
-#docker push theotherp/nzbhydra2
+docker push theotherp/nzbhydra2
