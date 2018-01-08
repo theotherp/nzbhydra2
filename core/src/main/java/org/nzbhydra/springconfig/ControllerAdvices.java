f524ab3183e68fec06fa060c5b6c7e4c35084e65
theotherp@gmx.de <SFSu3uR#qe&n#Y7lBC*J>
2018-01-08 22:54:14 +0100

    Make output format case sensitive

--- core/src/main/java/org/nzbhydra/springconfig/ControllerAdvices.java
+++ core/src/main/java/org/nzbhydra/springconfig/ControllerAdvices.java
@@ -1,6 +1,7 @@
 package org.nzbhydra.springconfig;
 
 import org.nzbhydra.mapping.newznab.ActionAttribute;
+import org.nzbhydra.mapping.newznab.OutputType;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 
@@ -10,5 +11,6 @@ public class ControllerAdvices {
     @InitBinder
     public void initBinder(WebDataBinder binder) {
         binder.registerCustomEditor(ActionAttribute.class, new EnumCaseInsensitiveConverter<>(ActionAttribute.class));
+        binder.registerCustomEditor(OutputType.class, new EnumCaseInsensitiveConverter<>(OutputType.class));
     }
 }
