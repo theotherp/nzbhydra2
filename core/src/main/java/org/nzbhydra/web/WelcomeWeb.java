236d76084bdfe0c9433e73e79b2c8254008a4e44
theotherp@gmx.de <SFSu3uR#qe&n#Y7lBC*J>
2018-01-18 17:15:06 +0100

    Lower role for welcomeshown to user

--- core/src/main/java/org/nzbhydra/web/WelcomeWeb.java
+++ core/src/main/java/org/nzbhydra/web/WelcomeWeb.java
@@ -4,6 +4,7 @@ import org.nzbhydra.config.ConfigProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,14 +20,14 @@ public class WelcomeWeb {
 
     private static final Logger logger = LoggerFactory.getLogger(WelcomeWeb.class);
 
-    @Secured({"ROLE_ADMIN"})
+    @Secured({"ROLE_USER"})
     @RequestMapping(value = "/internalapi/welcomeshown", method = RequestMethod.GET)
     public Boolean logfileContent() {
         return configProvider.getBaseConfig().getMain().isWelcomeShown();
     }
 
-    @Secured({"ROLE_ADMIN"})
-    @RequestMapping(value = "/internalapi/welcomeshown", method = RequestMethod.PUT)
+    @Secured({"ROLE_USER"})
+    @RequestMapping(value = "/internalapi/welcomeshown", method = RequestMethod.PUT, consumes = MediaType.ALL_VALUE)
     public void setWelcomeShown() throws IOException {
         logger.debug("Welcome screen was shown");
         configProvider.getBaseConfig().getMain().setWelcomeShown(true);
