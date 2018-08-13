307dec661f232ccf4c8f4787399a59ff76435400
TheOtherP <theotherp@gmx.de>
2018-08-13 16:43:04 +0200

    Fix Hikari

--- core/src/main/java/org/nzbhydra/springconfig/AppConfig.java
+++ core/src/main/java/org/nzbhydra/springconfig/AppConfig.java
@@ -4,13 +4,9 @@ import org.nzbhydra.okhttp.HydraOkHttp3ClientHttpRequestFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.client.RestTemplate;
-
-import javax.sql.DataSource;
 
 
 @EnableAsync
@@ -31,23 +27,6 @@ public class AppConfig {
         restTemplate.setRequestFactory(factory);
         return restTemplate;
     }
-
-    //https://stackoverflow.com/a/49141541
-    @Primary
-    @Bean
-    public DataSource customDataSource() {
-
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(env.getProperty("spring.datasource.driverClassName"));
-        dataSource.setUrl(env.getProperty("spring.datasource.url"));
-        dataSource.setUsername(env.getProperty("spring.datasource.username"));
-        dataSource.setPassword(env.getProperty("spring.datasource.password"));
-
-        return dataSource;
-
-    }
-
-
 
 
     //TODO: What was this for???
