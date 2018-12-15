3f637c534d6d3eb8a5c7de05ce8664ab54bc037a
TheOtherP <theotherp@gmx.de>
2018-12-15 10:28:15 +0100

    Try to fix circleci build

--- other/mockserver/src/test/java/org/nzbhydra/MockserverApplicationTests.java
+++ other/mockserver/src/test/java/org/nzbhydra/MockserverApplicationTests.java
@@ -2,11 +2,12 @@ package org.nzbhydra;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.nzbhydra.mockserver.MockserverApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = MockserverApplication.class)
 public class MockserverApplicationTests {
 
     @Test
