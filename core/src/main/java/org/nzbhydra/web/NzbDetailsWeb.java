224082b78effc8785091cc7f789fd4ed03fa8199
TheOtherP <theotherp@gmx.de>
2019-01-02 17:03:07 +0100

    Use dereferrer for /details/<gui> link. Closes #279

--- core/src/main/java/org/nzbhydra/web/NzbDetailsWeb.java
+++ core/src/main/java/org/nzbhydra/web/NzbDetailsWeb.java
@@ -16,6 +16,8 @@
 
 package org.nzbhydra.web;
 
+import com.google.common.base.Strings;
+import org.nzbhydra.config.ConfigProvider;
 import org.nzbhydra.searching.db.SearchResultRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,11 +29,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.view.RedirectView;
 
+import java.util.Optional;
+
 @Controller
 public class NzbDetailsWeb {
 
     @Autowired
     private SearchResultRepository searchResultRepository;
+    @Autowired
+    private ConfigProvider configProvider;
 
     private static final Logger logger = LoggerFactory.getLogger(NzbDetailsWeb.class);
 
@@ -40,6 +46,10 @@ public class NzbDetailsWeb {
     public RedirectView details(@PathVariable("guid") long guid) {
         RedirectView redirectView = new RedirectView();
         String url = searchResultRepository.findById(guid).get().getDetails();
+        Optional<String> derefererOptional = configProvider.getBaseConfig().getMain().getDereferer();
+        if (derefererOptional.isPresent() && !Strings.isNullOrEmpty(derefererOptional.get())) {
+            url = derefererOptional.get().replace("$s", url);
+        }
         redirectView.setUrl(url);
         logger.debug("Redirecting to {} for GUID {}", url, guid);
         return redirectView;
