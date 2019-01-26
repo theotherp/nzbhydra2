49a8e20dd1edb00f446cc74a3bd458371587a1c2
TheOtherP <theotherp@gmx.de>
2019-01-26 13:11:16 +0100

    Update to Spring Boot 2.1 and Spring 5.1. Support Java 11.

--- core/src/main/java/org/nzbhydra/auth/HydraGlobalMethodSecurityConfiguration.java
+++ core/src/main/java/org/nzbhydra/auth/HydraGlobalMethodSecurityConfiguration.java
@@ -1,3 +1,19 @@
+/*
+ *  (C) Copyright 2017 TheOtherP (theotherp@gmx.de)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package org.nzbhydra.auth;
 
 import org.nzbhydra.config.BaseConfig;
@@ -5,35 +21,36 @@ import org.nzbhydra.config.auth.AuthType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.annotation.SecuredAnnotationSecurityMetadataSource;
+import org.springframework.security.access.method.DelegatingMethodSecurityMetadataSource;
 import org.springframework.security.access.method.MethodSecurityMetadataSource;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 @EnableGlobalMethodSecurity
 @Configuration
 public class HydraGlobalMethodSecurityConfiguration extends GlobalMethodSecurityConfiguration {
 
-    private static final Logger logger = LoggerFactory.getLogger(HydraGlobalMethodSecurityConfiguration.class);
+    private static final Logger hydraLogger = LoggerFactory.getLogger(HydraGlobalMethodSecurityConfiguration.class);
 
     @Autowired
     private BaseConfig baseConfig;
 
-    @Override
-    protected AuthenticationManager authenticationManager() throws Exception {
-        return super.authenticationManager();
-    }
+    @Bean
+    public MethodSecurityMetadataSource methodSecurityMetadataSource() {
+        List<MethodSecurityMetadataSource> sources = new ArrayList<>();
 
-    @Override
-    protected MethodSecurityMetadataSource customMethodSecurityMetadataSource() {
         if (baseConfig.getAuth().getAuthType() != AuthType.NONE) {
-            logger.info("Enabling auth type " + baseConfig.getAuth().getAuthType());
-            return new SecuredAnnotationSecurityMetadataSource();
+            hydraLogger.info("Enabling auth type " + baseConfig.getAuth().getAuthType());
+            sources.add(new SecuredAnnotationSecurityMetadataSource());
         }
-        return null;
+
+        return new DelegatingMethodSecurityMetadataSource(sources);
     }
-
-
 }
