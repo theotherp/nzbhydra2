7ed4ea0e0e8186fc2fe52f10c96d0b34e6501164
benoit charret <benoit.charret@axioconsulting.fr>
2019-02-13 09:15:27 +0100

    fix unit test

--- core/src/test/java/org/nzbhydra/indexers/NzbIndexTest.java
+++ core/src/test/java/org/nzbhydra/indexers/NzbIndexTest.java
@@ -21,6 +21,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
 import static org.junit.Assert.assertThat;
 
 
@@ -41,17 +42,14 @@ public class NzbIndexTest {
     @Test
     public void shouldParseRows() throws Exception {
         Instant now = Instant.now();
-        String enclosureUrl = "http://nzbindex.com/download/164950363/Watchers.of.the.Universe.S02E09.1080p.WEB-DL.DD5.1.AAC2.0.H.264-YFN-0030-Watchers.of.the.Universe.S02E09.Cant.Get.You.out.of.My.Head.1080p.WEB-DL.nzb";
-        String link = "http://nzbindex.com/release/164950363/Watchers.of.the.Universe.S02E09.1080p.WEB-DL.DD5.1.AAC2.0.H.264-YFN-0030-Watchers.of.the.Universe.S02E09.Cant.Get.You.out.of.My.Head.1080p.WEB-DL.nzb";
+        String enclosureUrl = "http://nzbindex.com/download/164950363/";
+        String link = "http://nzbindex.com/download/164950363/";
+        String guid = "http://nzbindex.com/collection/164950363/";
         NewznabXmlRoot root = RssBuilder.builder().items(
                 Arrays.asList(
                         RssItemBuilder
                                 .builder("[ Watchers.of.the.Universe.S02E09.1080p.WEB-DL.DD5.1.AAC2.0.H.264-YFN ] - [00/30] - \"Watchers.of.the.Universe.S02E09.Cant.Get.You.out.of.My.Head.1080p.WEB-DL.DD5.1.AAC2.0.H.264-YFN.nzb\" yEnc\n")
                                 .link(link)
-                                .description("<![CDATA[\n" +
-                                        "<p><font color=\"gray\">alt.binaries.hdtv.x264</font><br /> <b>1.01 GB</b><br /> 7 hours<br /> <font color=\"#3DA233\">31 files (1405 parts)</font> <font color=\"gray\">by s@nd.p (SP)</font><br /> <font color=\"#E2A910\"> 1 NFO | 9 PAR2 | 1 NZB | 19 ARCHIVE</font> - <a href=\"http://nzbindex.com/nfo/164950363/Watchers.of.the.Universe.S02E09.1080p.WEB-DL.DD5.1.AAC2.0.H.264-YFN-0030-Watchers.of.the.Universe.S02E09.Cant.Get.You.out.of.My.Head.1080p.WEB-DL.nzb/?q=\" target=\"_blank\">View NFO</a></p>\n" +
-                                        "]]>")
-                                .category("alt.binaries.hdtv.x264")
                                 .pubDate(now)
                                 .rssGuid(new NewznabXmlGuid(link, true))
                                 .enclosure(new NewznabXmlEnclosure(enclosureUrl, 1089197181L, "application/x-nzb"))
@@ -60,14 +58,13 @@ public class NzbIndexTest {
         assertThat(items.size(), is(1));
         SearchResultItem item = items.get(0);
 
-        assertThat(item.getTitle(), is("Watchers.of.the.Universe.S02E09.1080p.WEB-DL.DD5.1.AAC2.0.H.264-YFN-0030-Watchers.of.the.Universe.S02E09.Cant.Get.You.out.of.My.Head.1080p.WEB-DL"));
-        assertThat(item.getGroup().get(), is("alt.binaries.hdtv.x264"));
+        assertThat(item.getTitle(), is("164950363"));
         assertThat(item.getPubDate(), is(now));
         assertThat(item.isAgePrecise(), is(true));
         assertThat(item.getSize(), is(1089197181L));
         assertThat(item.getIndexerGuid(), is("164950363"));
         assertThat(item.getDownloadType(), is(DownloadType.NZB));
-        assertThat(item.getHasNfo(), is(HasNfo.YES));
+        assertThat(item.getHasNfo(), is(HasNfo.NO));
 
     }
 
