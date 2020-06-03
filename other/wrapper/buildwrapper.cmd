1d7ece41798c34cef012e2909343722beceed5c0
TheOtherP <theotherp@gmx.de>
2020-06-03 17:33:40 +0200

    Add version info to wrapper build script
    Closes #572

--- other/wrapper/buildwrapper.cmd
+++ other/wrapper/buildwrapper.cmd
@@ -2,17 +2,12 @@
 
 rem The windows docker (cdrx/pyinstaller-windows) produces unusable results
 
-echo todo Add exe details (author, etc)
-pause
-pause
-pause
-
 echo Running pyInstaller for windows wrapper
-c:\programme\Python27\Scripts\pyinstaller.exe -F nzbhydra2wrapper.py -n "NZBHydra2" -i nzbhydra.ico -w --clean --win-private-assemblies
+c:\programme\Python27\Scripts\pyinstaller.exe -F nzbhydra2wrapper.py -n "NZBHydra2" -i nzbhydra.ico -w --clean --win-private-assemblies --version-file=VersionInfo.txt
 copy dist\nzbhydra2.exe ..\..\releases\windows-release\include / y
 
 echo Running pyInstaller for windows console wrapper
-c:\programme\Python27\Scripts\pyinstaller.exe -F nzbhydra2wrapper.py -n "NZBHydra2 Console" -i nzbhydra.ico  --clean --win-private-assemblies
+c:\programme\Python27\Scripts\pyinstaller.exe -F nzbhydra2wrapper.py -n "NZBHydra2 Console" -i nzbhydra.ico  --clean --win-private-assemblies  --version-file=VersionInfoConsole.txt
 copy "dist\nzbhydra2 console.exe" ..\..\releases\windows-release\include / y
 
 echo Running docker for linux wrapper
