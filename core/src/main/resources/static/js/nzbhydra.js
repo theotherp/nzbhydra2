fc75e711d82dadbdb5231ae7f4ea436351a80ee8
TheOtherP <theotherp@gmx.de>
2020-11-20 20:23:16 +0100

    Celebration time, come on!

--- core/src/main/resources/static/js/nzbhydra.js
+++ core/src/main/resources/static/js/nzbhydra.js
@@ -2299,13 +2299,25 @@ angular
     .directive('footer', footer);
 
 function footer() {
-    controller.$inject = ["$scope", "$http", "ConfigService", "bootstrapped"];
+    controller.$inject = ["$scope", "$http", "$uibModal", "ConfigService", "GenericStorageService", "bootstrapped"];
     return {
         templateUrl: 'static/html/directives/footer.html',
         controller: controller
     };
 
-    function controller($scope, $http, ConfigService, bootstrapped) {
+    function controller($scope, $http, $uibModal, ConfigService, GenericStorageService, bootstrapped) {
+
+        var birthday = moment("2020-12-06");
+        //Hacky but I'm too dumb to do it properly and it works
+        if (birthday.dayOfYear() === moment().dayOfYear() && birthday.year() === moment().year()) {
+
+            GenericStorageService.get("birthdaySeen", true).then(function (birthdaySeen) {
+                if (birthdaySeen.data === null || !birthdaySeen.data) {
+                    showCelebration($uibModal, $scope, GenericStorageService);
+                    GenericStorageService.put("birthdaySeen", true, true);
+                }
+            });
+        }
 
         $scope.updateFooterBottom = 0;
 
@@ -2369,7 +2381,109 @@ function footer() {
     }
 }
 
+function showCelebration($uibModal, $scope, GenericStorageService) {
+    var params = {
+        size: "lg",
+        backdrop: 'static',
+        templateUrl: "static/html/celebration-modal.html",
+        controller: function ($scope, $uibModalInstance) {
 
+            $scope.close = function () {
+                $uibModalInstance.dismiss();
+            };
+
+            setTimeout(function () {
+
+                const max_fireworks = 5,
+                    max_sparks = 50;
+                let canvas = document.getElementById('myCanvas');
+                var dimension = [document.documentElement.clientWidth, document.documentElement.clientHeight];
+
+                canvas.width = dimension[0];
+                canvas.height = dimension[1];
+                canvas.style.display = "block";
+                let context = canvas.getContext('2d');
+                let fireworks = [];
+
+                for (let i = 0; i < max_fireworks; i++) {
+                    let firework = {
+                        sparks: []
+                    };
+                    for (let n = 0; n < max_sparks; n++) {
+                        let spark = {
+                            vx: Math.random() * 5 + .5,
+                            vy: Math.random() * 5 + .5,
+                            weight: Math.random() * .3 + .03,
+                            red: Math.floor(Math.random() * 2),
+                            green: Math.floor(Math.random() * 2),
+                            blue: Math.floor(Math.random() * 2)
+                        };
+                        if (Math.random() > .5) spark.vx = -spark.vx;
+                        if (Math.random() > .5) spark.vy = -spark.vy;
+                        firework.sparks.push(spark);
+                    }
+                    fireworks.push(firework);
+                    resetFirework(firework);
+                }
+                window.requestAnimationFrame(explode);
+
+                function resetFirework(firework) {
+                    firework.x = Math.floor(Math.random() * canvas.width);
+                    firework.y = canvas.height;
+                    firework.age = 0;
+                    firework.phase = 'fly';
+                }
+
+                function explode() {
+                    context.clearRect(0, 0, canvas.width, canvas.height);
+                    fireworks.forEach((firework, index) => {
+                        if (firework.phase === 'explode') {
+                            firework.sparks.forEach((spark) => {
+                                for (let i = 0; i < 10; i++) {
+                                    let trailAge = firework.age + i;
+                                    let x = firework.x + spark.vx * trailAge;
+                                    let y = firework.y + spark.vy * trailAge + spark.weight * trailAge * spark.weight * trailAge;
+                                    let fade = i * 20 - firework.age * 2;
+                                    let r = Math.floor(spark.red * fade);
+                                    let g = Math.floor(spark.green * fade);
+                                    let b = Math.floor(spark.blue * fade);
+                                    context.beginPath();
+                                    context.fillStyle = 'rgba(' + r + ',' + g + ',' + b + ',1)';
+                                    context.rect(x, y, 4, 4);
+                                    context.fill();
+                                }
+                            });
+                            firework.age++;
+                            if (firework.age > 100 && Math.random() < .05) {
+                                resetFirework(firework);
+                            }
+                        } else {
+                            firework.y = firework.y - 10;
+                            for (let spark = 0; spark < 15; spark++) {
+                                context.beginPath();
+                                context.fillStyle = 'rgba(' + index * 50 + ',' + spark * 17 + ',0,1)';
+                                context.rect(firework.x + Math.random() * spark - spark / 2, firework.y + spark * 4, 4, 4);
+                                context.fill();
+                            }
+                            if (Math.random() < .001 || firework.y < 200) firework.phase = 'explode';
+                        }
+                    });
+                    window.requestAnimationFrame(explode);
+                }
+
+            }, 500);
+
+            $scope.$on("modal.closing", function () {
+                document.getElementById('myCanvas').style.display = "none";
+            });
+        }
+    };
+
+
+    $uibModal.open(params).result.then();
+
+
+}
 angular
     .module('nzbhydraApp').directive('focusOn', focusOn);
 
@@ -3469,7 +3583,6 @@ function addableNzb(DebugService) {
         };
     }
 }
-
 /*
  *  (C) Copyright 2017 TheOtherP (theotherp@posteo.net)
  *
