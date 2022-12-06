b9b604978d856ad27050d96c19888b769358d54c
TheOtherP <theotherp@gmx.de>
2022-12-06 17:43:33 +0100

    Fix build script to be run in linux

--- other/wrapper/buildwrapper.cmd
+++ other/wrapper/buildwrapper.cmd
@@ -10,8 +10,6 @@ echo Running pyInstaller for windows console wrapper
 c:\programme\Python27\Scripts\pyinstaller.exe -F nzbhydra2wrapper.py -n "NZBHydra2 Console" -i nzbhydra.ico  --clean --win-private-assemblies  --version-file=VersionInfoConsole.txt
 copy "dist\nzbhydra2 console.exe" ..\..\releases\windows-release\include / y
 
-echo Running docker for linux wrapper
-docker run -v "C:\Users\strat\IdeaProjects\nzbhydra2\other\wrapper:/src/" cdrx/pyinstaller-linux:python2
-
-echo Copying linux wrapper
-copy "dist\linux\NZBHydra2 Console" ..\..\releases\linux-release\include\nzbhydra2 / y
+echo Run in linux:
+echo docker run -v "$PWD:/src/" cdrx/pyinstaller-linux:python2
+echo cp dist/linux/NZBHydra2\ Console ../../releases/linux-release/include/nzbhydra2
