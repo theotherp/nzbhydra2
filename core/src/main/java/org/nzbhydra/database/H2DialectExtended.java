a8da35d85afa9e54a56e7123fb2770cb28a57165
TheOtherP <theotherp@gmx.de>
2022-12-17 09:43:43 +0100

    Fix h2 dialect version

--- core/src/main/java/org/nzbhydra/database/H2DialectExtended.java
+++ core/src/main/java/org/nzbhydra/database/H2DialectExtended.java
@@ -16,14 +16,31 @@
 
 package org.nzbhydra.database;
 
+import org.h2.engine.Constants;
 import org.hibernate.MappingException;
+import org.hibernate.dialect.DatabaseVersion;
 import org.hibernate.dialect.H2Dialect;
+import org.hibernate.dialect.SimpleDatabaseVersion;
 import org.hibernate.dialect.sequence.ANSISequenceSupport;
 import org.hibernate.dialect.sequence.SequenceSupport;
+import org.hibernate.engine.jdbc.dialect.spi.DialectResolutionInfo;
 import org.hibernate.tool.schema.extract.internal.SequenceInformationExtractorLegacyImpl;
 import org.hibernate.tool.schema.extract.spi.SequenceInformationExtractor;
 
 public class H2DialectExtended extends H2Dialect {
+
+    public H2DialectExtended(DialectResolutionInfo info) {
+        this();
+    }
+
+    public H2DialectExtended() {
+        //Instance is created without DatabaseVersion info (or ZERO)
+        super(new SimpleDatabaseVersion(Constants.VERSION_MAJOR, Constants.VERSION_MINOR, Constants.BUILD_ID));
+    }
+
+    public H2DialectExtended(DatabaseVersion version) {
+        this();
+    }
 
     @Override
     public String toBooleanValueString(boolean bool) {
