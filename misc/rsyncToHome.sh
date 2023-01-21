70eed43be356bbc7b7fd53fa82f062377917600a
TheOtherP <theotherp@gmx.de>
2023-01-21 11:46:53 +0100

    Include service scripts and all wrapper files in generic release

---
+++ misc/rsyncToHome.sh
@@ -0,0 +1,1 @@
+rsync -rvu --exclude "target" --exclude "bower_components" --exclude "node_modules" --exclude ".git" --exclude ".idea" --exclude "results" --exclude "*.db" --exclude "venv*" . ~/nzbhydra2/
