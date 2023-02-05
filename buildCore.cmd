14630bf9eb330abd205c7e423c1fe6709faadd0f
TheOtherP <theotherp@gmx.de>
2023-02-05 15:46:18 +0100

    Use VS 2022

--- buildCore.cmd
+++ buildCore.cmd
@@ -2,7 +2,7 @@
 
 setlocal
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 
 set path=c:\Programme\graalvm-ce-java17-22.2.0\bin\;%PATH%;c:\Programme\graalvm-ce-java17-22.2.0\bin\
 set java_home=c:\Programme\graalvm-ce-java17-22.2.0\
