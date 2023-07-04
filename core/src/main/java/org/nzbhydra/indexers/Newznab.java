ead61e78cca02ec906399b4057ce7c6861f3a3e1
TheOtherP <theotherp@gmx.de>
2023-07-04 19:19:15 +0200

    Filter out no-cover.jpg covers

--- core/src/main/java/org/nzbhydra/indexers/Newznab.java
+++ core/src/main/java/org/nzbhydra/indexers/Newznab.java
@@ -625,7 +625,7 @@ public class Newznab extends Indexer<Xml> {
             searchResultItem.setHasNfo(HasNfo.YES);
         }
         Arrays.asList("coverurl", "cover").forEach(x -> {
-            if (attributes.containsKey(x) && !attributes.get(x).equals("not available") && (attributes.get(x).toLowerCase().endsWith(".jpg") || attributes.get(x).toLowerCase().endsWith(".jpeg") || attributes.get(x).toLowerCase().endsWith(".png"))) {
+            if (attributes.containsKey(x) && !attributes.get(x).equals("not available") && !attributes.get(x).equals("no-cover") && (attributes.get(x).toLowerCase().endsWith(".jpg") || attributes.get(x).toLowerCase().endsWith(".jpeg") || attributes.get(x).toLowerCase().endsWith(".png"))) {
                 searchResultItem.setCover(attributes.get(x));
             }
         });
