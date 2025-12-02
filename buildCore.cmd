35dee7d9f6aa9d3d3abb524751fb5014b9caf49c
TheOtherP <theotherp@gmx.de>
2025-12-02 13:13:03 +0100

    Fix SerializedModelConfigurator in reflect-config.json

--- buildCore.cmd
+++ buildCore.cmd
@@ -7,6 +7,10 @@ call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build
 set path=c:\Programme\graalvm\graalvm-community-openjdk-22.0.1+8.1\bin\;%PATH%;c:\Programme\graalvm\graalvm-community-openjdk-22.0.1+8.1\bin\
 set java_home=c:\Programme\graalvm\graalvm-community-openjdk-22.0.1+8.1\
 set HYDRA_NATIVE_BUILD=true
-call mvn -pl org.nzbhydra:core -Pnative clean native:compile -DskipTests
+
+set PROFILES=native
+if "%1"=="checkReflection" set PROFILES=native,strictReflection
+echo "Using profiles: %PROFILES%"
+call mvn -pl org.nzbhydra:core -P%PROFILES% clean native:compile -DskipTests
 
 endlocal
