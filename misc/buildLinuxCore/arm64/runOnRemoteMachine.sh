0bb6b967791e23c54051df25f835662ac083c206
TheOtherP <theotherp@gmx.de>
2026-01-05 10:19:39 +0100

    Clean up .m2 on oracle, but update release files

--- misc/buildLinuxCore/arm64/runOnRemoteMachine.sh
+++ misc/buildLinuxCore/arm64/runOnRemoteMachine.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Is executed on the build machine to build the linux executable
-
+sudo rm -rf  /home/build/.m2/repository/org/nzbhydra*
 docker run -v ~/nzbhydra2/:/nzbhydra2:rw -v ~/.m2/repository:/root/.m2/repository:rw --rm hydrabuild:latest
