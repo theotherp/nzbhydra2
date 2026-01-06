5e33ecd432911c6210cd7dec3e3c35dc5403d3d1
TheOtherP <theotherp@gmx.de>
2026-01-06 12:06:02 +0100

    Don't build other modules on linux

--- misc/buildLinuxCore/amd64/buildInContainer.sh
+++ misc/buildLinuxCore/amd64/buildInContainer.sh
@@ -7,8 +7,8 @@
 cd /nzbhydra2 || exit
 #clean so that if the build fails we won't use the old results
 rm -rf core/target
-mvn -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn --batch-mode clean install -pl \!org.nzbhydra:linux-amd64-release,!org.nzbhydra:linux-arm64-release,\!org.nzbhydra:windows-release,\!org.nzbhydra:generic-release -DskipTests -T 1C
-mvn -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -pl org.nzbhydra:core -Pnative clean native:compile -DskipTests
+mvn --batch-mode -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn --batch-mode clean install -pl \!org.nzbhydra:linux-amd64-release,!org.nzbhydra:linux-arm64-release,\!org.nzbhydra:windows-release,\!org.nzbhydra:generic-release,\!org.nzbhydra:github-release-plugin,\!org.nzbhydra:discordreleaser -DskipTests -T 1C
+mvn --batch-mode -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn -pl org.nzbhydra:core -Pnative clean native:compile -DskipTests
 /upx-4.2.4-amd64_linux/upx -3 core/target/core
 #Because docker is run as root the files are written to the host file system as root
 chmod o+rwx -R .
