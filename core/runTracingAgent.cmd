00beef07a63fc1b6ad33be21213a9778ea985eba
TheOtherP <theotherp@gmx.de>
2026-07-20 21:08:19 +0200

    Extended reachability-metadata.json with traced data

--- core/runTracingAgent.cmd
+++ core/runTracingAgent.cmd
@@ -8,6 +8,6 @@ set path=c:\Programme\graalvm\graalvm-community-25.1.3+9.1\bin\;%PATH%;c:\Progra
 set java_home=c:\Programme\graalvm\graalvm-community-25.1.3+9.1\
 set HYDRA_NATIVE_BUILD=true
 cd target
-java -DspringAot=true -agentlib:native-image-agent=config-output-dir=hints -jar core-5.0.0-SNAPSHOT-exec.jar directstart
+java -DspringAot=true -agentlib:native-image-agent=config-output-dir=hints -jar core-8.9.1-SNAPSHOT-exec.jar directstart
 
 endlocal
